30de694a5a4f6fd993abc6a5fddc4f5f4227f422
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-04-24 09:31:39 -0600

    Add: connection for salary search method

--- app/services/teleport_api_service.rb
+++ app/services/teleport_api_service.rb
@@ -1,11 +1,11 @@
 class TeleportApiService
-  def search_by_name(city)
-    conn.get('/api/cities/') do |req|
-      req.headers['Accept'] = 'application/vnd.teleport.v1+json'
-      req.headers['Content-Type'] = 'application/json'
-      req.params['search'] = "#{city}"
-    end
-  end
+  # def search_by_name(city)
+  #   conn.get('/api/cities/') do |req|
+  #     req.headers['Accept'] = 'application/vnd.teleport.v1+json'
+  #     req.headers['Content-Type'] = 'application/json'
+  #     req.params['search'] = "#{city}"
+  #   end
+  # end
 
   def conn
     Faraday.new(url: "https://api.teleport.org") 
--- spec/services/teleport_api_service_spec.rb
+++ spec/services/teleport_api_service_spec.rb
@@ -6,46 +6,46 @@ RSpec.describe TeleportApiService do
       before do
         @city_params = "bozeman"
       end
+
+    # context '#search_by_name(city)' do
+    #   before do
+    #     @city_params = "bozeman"
+    #   end
       
-      it 'can successfully make a request by city name' do
-        VCR.use_cassette('GET search_by_name(city)') do
-          response = TeleportApiService.new.search_by_name(@city_params)
+    #   it 'can successfully make a request by city name' do
+    #     VCR.use_cassette('GET search_by_name(city)') do
+    #       response = TeleportApiService.new.search_by_name(@city_params)
     
-          expect(response.status).to eq(200)
+    #       expect(response.status).to eq(200)
 
-          json = JSON.parse(response.body, symbolize_names: true)
+    #       json = JSON.parse(response.body, symbolize_names: true)
           
-          expect(json).to have_key(:_embedded)
-          expect(json[:_embedded]).to be_a Hash
-          expect(json[:_embedded]).to have_key(:"city:search-results")
-          expect(json[:_embedded][:"city:search-results"]).to be_an Array
+    #       expect(json).to have_key(:_embedded)
+    #       expect(json[:_embedded]).to be_a Hash
+    #       expect(json[:_embedded]).to have_key(:"city:search-results")
+    #       expect(json[:_embedded][:"city:search-results"]).to be_an Array
           
-          search_results = json[:_embedded][:"city:search-results"]
+    #       search_results = json[:_embedded][:"city:search-results"]
 
-          search_results.each do |result|
-            require 'pry'; binding.pry
-            expect(result).to be_a Hash
-            expect(result[:_links]).to be_a Hash
-            expect(result[:links].keys).to eq
+    #       search_results.each do |result|
+    #         expect(result).to be_a Hash
+    #         expect(result.keys).to eq([:_links, :matching_alternate_names, :matching_full_name])
+            
+    #         expect(result[:_links]).to be_a Hash
+    #         expect(result[:_links][:"city:item"]).to be_a Hash
+    #         expect(result[:_links][:"city:item"][:href]).to be_a String
+            
+    #         expect(result[:matching_alternate_names]).to be_an Array
 
-          end
-          expect(search_result)
-          expect(json.keys).to include(:options)
-          expect(json[:options][:ignoreLatLngInput]).to eq(false)
+    #         result[:matching_alternate_names].each do |name|
+    #           expect(name).to be_a Hash
+    #           expect(name[:name]).to be_a String
+    #         end
 
-          expect(json[:results]).to be_an Array
-          expect(json[:results]).to be_an Array
-          
-          expect(json[:results][0][:providedLocation]).to be_a Hash
-          expect(json[:results][0][:locations]).to be_an Array
-          
-          location_info = json[:results][0][:locations].first
-          
-          expect(location_info[:latLng]).to be_a Hash
-          expect(location_info[:latLng][:lat]).to be_a Float
-          expect(location_info[:latLng][:lng]).to be_a Float
-        end
-      end
-    end
+    #         expect(result[:matching_full_name]).to be_a String
+    #       end
+    #     end
+    #   end
+    # end
   end
 end
