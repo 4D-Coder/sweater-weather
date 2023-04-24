7345a85a1d4345f5b89606142bb41156176fcd3b
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-04-24 15:27:20 -0600

    Add: user migration and tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  puts "Running tests in #{Rails.env} environment..."
+
+  before(:each) do
+    @user = create(:user)
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should have_secure_password }
+  end
+  
+  it 'secure_password' do
+    expect(@user.password_digest).to_not eq('password123')
+    expect(@user).to_not have_attribute(:password)
+  end 
+end
