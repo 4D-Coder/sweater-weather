3773c69ce85e7fe5b66ff0322868b88fb225d026
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-04-25 12:00:29 -0600

    Add: finalize testing for activities_facade

--- spec/facades/activities_facade_spec.rb
+++ spec/facades/activities_facade_spec.rb
@@ -4,15 +4,18 @@ RSpec.describe ActivitiesFacade do
   describe ".instance methods", :vcr do 
     describe  "#get_activities" do 
       before(:each) do 
-        @activities = ActivitiesFacade.new.get_activities("chicago,il")
+        @param = "chicago,il"
+        @activities = ActivitiesFacade.new.get_activities(@param)
       end
 
       it "returns the value of a PORO containing forecast and suitable activities for it" do
         expect(@activities).to be_a Activities
         expect(@activities.id).to be_nil
-        expect(@activities.destination).to eq("chicago,il")
+        expect(@activities.destination).to eq(@param)
         expect(@activities.activities).to be_a Hash
-     
+
+        expect(@activities.activities.first[1][:type]).to eq("relaxation")
+
         expect(@activities.forecast).to be_a Hash
         expect(@activities.forecast).to have_key(:temperature)
 
--- spec/requests/api/v0/activities_request_spec.rb
+++ spec/requests/api/v0/activities_request_spec.rb
@@ -4,15 +4,34 @@ RSpec.describe "Activities API", :vcr do
   describe "when successful" do 
     before(:each) do 
       get "/api/v0/activities?destination=chicago,il"
-      @response = JSON.parse(response.body, symbolize_names: true)
     end
-
+    
     it "returns the forecast and two activities suitable for it" do
-      require 'pry'; binding.pry
-      expect(@response.status).to eq(200)
+      expect(response.status).to eq(200)
+      
+      @response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(@response).to have_key(:data)
       expect(@response[:data]).to be_a Hash
+
       expect(@response[:data]).to have_key(:attributes)
       expect(@response[:data][:attributes]).to be_a Hash
+
+      attributes = @response[:data][:attributes]
+
+      expect(attributes[:destination]).to be_a String
+      expect(attributes[:forecast]).to be_a Hash
+      expect(attributes[:forecast][:summary]).to be_a String
+      expect(attributes[:forecast][:temperature]).to be_a String
+      expect(attributes[:activities]).to be_a Hash
+      
+      attributes[:activities].each do |k, v|
+        expect(k.to_s).to be_a String
+        expect(v).to be_a Hash
+        expect(v[:type]).to be_a String
+        expect(v[:participants]).to be_an Integer
+        expect(v[:price]).to be_an(Integer).or be_a(Float)
+      end
     end
   end
 end
